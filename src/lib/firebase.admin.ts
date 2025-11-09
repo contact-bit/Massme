2a443797dd8bfb7f4777e3eb194b75b1b985d338
Massme <contact@hdconnects.com>
2025-11-09 16:00:04 +0100

    🔥 remove local serviceAccountKey + use env variables for Firebase

--- src/lib/firebase.admin.ts
+++ src/lib/firebase.admin.ts
@@ -1,11 +1,15 @@
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import serviceAccount from "../../serviceAccountKey.json" assert { type: "json" };
 
-const app = !getApps().length
-  ? initializeApp({
-      credential: cert(serviceAccount as any),
-    })
-  : getApps()[0];
+const serviceAccount = {
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+  clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+};
+
+const app =
+  !getApps().length
+    ? initializeApp({ credential: cert(serviceAccount as any) })
+    : getApps()[0];
 
 export const adminDB = getFirestore(app);
